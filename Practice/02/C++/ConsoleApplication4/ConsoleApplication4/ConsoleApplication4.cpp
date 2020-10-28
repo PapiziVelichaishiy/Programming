44678bc64c01b1581dcab8cfb1a63b7f248f146d
viktor.vnukov <vn-vnukov@inbox.ru>
2020-10-28 18:36:47 +0300

    first commit

---
+++ Practice/02/C++/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int a = 3;
+    int b = 3.14;
+    double c = 3;
+    double z = 3.14;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << z << endl;
+}
